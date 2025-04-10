43efd829ec8c844b0047470dfa2f1097134301d1
ErnestoAcm <l20170741@culiacan.tecnm.mx>
2025-04-10 02:44:21 -0700

    mejora del formato

--- app/Models/autenticacion.php
+++ app/Models/autenticacion.php
@@ -98,5 +98,25 @@ class autenticacion
         return;
     }
 
+    public function formato($request)
+    {
+        $request->validate([
+            'correo' => [
+                'required',
+                'email',
+                'max:100',
+                'regex:/^[a-zA-Z0-9._%+-]{1,50}@[a-zA-Z0-9.-]{1,50}\.com$/'
+            ],
+            'nip' => [
+                'required',
+                'min:12',
+                'regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{12,}$/'
+            ],
+        ], [
+            'correo.regex' => 'El correo debe tener un máximo de 50 caracteres antes y después del @, y terminar con ".com".',
+            'nip.regex' => 'La contraseña debe tener al menos 12 caracteres, incluyendo una mayúscula, una minúscula, un número y un símbolo.',
+        ]);
+    }
+
 
 }
